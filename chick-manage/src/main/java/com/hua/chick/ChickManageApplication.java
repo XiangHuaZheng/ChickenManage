ef50128bcb2ab5a82a11a72d4e2c86e4be77a526
xiangHuaZZ <912358463@qq.com>
2020-10-01 02:14:46 +0800

    搭建后台项目

---
+++ chick-manage/src/main/java/com/hua/chick/ChickManageApplication.java
@@ -0,0 +1,13 @@
+package com.hua.chick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChickManageApplication.class, args);
+    }
+
+}
